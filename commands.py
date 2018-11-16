dcb510f305707b4bbef4e3907e1cd99353669eca
Aeolus1364 <thetesseractsshadow@gmail.com>
2018-11-16 14:51:48 -0500

    moving isolang development to its own repo

---
+++ commands.py
@@ -0,0 +1,69 @@
+class Command:
+    def __init__(self, num_args, priority, return_type):
+        self.num_args = num_args  # number of arguments it takes
+        self.priority = priority  # priority level of a command
+        self.args = []  # list of actual arguments of a function
+        self.active = True
+        self.return_type = return_type
+
+    def argument(self, arg):
+        self.args.append(arg)
+        self.num_args -= 1
+
+    def complete(self):
+        if self.num_args < 0:
+            print("Error: number of arguments less than zero")
+        return self.num_args == 0
+
+
+class Set(Command):  # sets variable value in var stream
+    def __init__(self, priority):
+        super().__init__(2, priority, "var")
+        # arg 0: var name
+        # arg 1: var value
+
+    def evaluate(self, var):
+        var[self.args[0]] = self.args[1]
+        return var
+
+
+class Get(Command):  # gets variable value from var stream
+    def __init__(self, priority):
+        super().__init__(1, priority, "value")
+        # arg 0: var name
+
+    def evaluate(self,  var):
+        return var[self.args[0]]  # returns arg value from var dict
+
+
+class Print(Command):
+    def __init__(self, priority):
+        super().__init__(1, priority, "none")
+        # arg 0: prints value
+
+    def evaluate(self):
+        print(self.args[0])
+
+
+class Goto(Command):
+    def __init__(self, priority):
+        super().__init__(1, priority, "current")
+
+    def evaluate(self, current):
+        return self.args[0]
+
+
+class Add(Command):
+    def __init__(self, priority):
+        super().__init__(2, priority, "value")
+
+    def evaluate(self, var):
+        return self.args[0] + self.args[1]
+
+
+class Default(Command):
+    def __init__(self, priority):
+        super().__init__(0, priority, "")
+
+    def evaluate(self):
+        pass
---
+++ interpreter.py
@@ -0,0 +1,122 @@
+import commands
+
+
+class Interpreter:
+    def __init__(self, file):
+        self.priority = 0  # current priority level
+        self.current = 0  # current packet
+
+        self.stream = []
+        self.load(file)
+        # print(self.stream)
+        self.var = {}
+        self.cmds = []  # highest priority command receives arguments
+
+        self.list_cmd = open("cmd_list.txt", "r").read().split()  # list of all commands
+        self.holder = None  # holds returned values until the next command activates
+        self.resolve = True
+        self.halt = False
+
+    def load(self, file):
+        load_stream = []
+        with open(file, "r") as f:  # opens file
+            text = f.read()
+            for w in text.split():  # iterates through all words in text
+                if w.isdigit():  # if string is a valid int, converted
+                    load_stream.append(int(w))
+                else:  # otherwise kept the same
+                    load_stream.append(w)
+        self.stream = load_stream
+
+    def step(self):
+
+        packet = self.stream[self.current]  # packet is the current item in the stream
+        self.current += 1
+        packtype = self.get_type(packet)  # packets can be values or commands
+
+        if self.priority:  # if there is a priority
+            active = self.cmds[self.priority - 1]  # command with that priority set to active
+        else:
+            active = None
+
+        if packtype == "cmd":
+            self.priority += 1  # new command gets 1 higher priority
+            self.cmds.append(self.get_cmd(packet)(self.priority))  # adds new active command
+
+        elif packtype == "val":
+            active.argument(packet)  # sends argument to priority command
+
+        while self.resolve:
+            active = self.cmds[self.priority - 1]
+            self.resolve = False
+
+            if self.holder:
+                active.argument(self.holder)
+                self.holder = None
+
+            if active.complete():  # on command completion
+                type = active.return_type  # get return type
+                # passes in requested data to modify
+                if type == "var":  # changing var stream
+                    self.var = active.evaluate(self.var)
+                elif type == "stream":  # changing packet stream
+                    self.stream = active.evaluate(self.stream)
+                elif type == "current":  # changing current packet
+                    self.current = active.evaluate(self.current)
+                elif type == "value":  # returns argument for next command
+                    self.holder = active.evaluate(self.var)
+                    self.resolve = True
+                elif type == "none":
+                    active.evaluate()
+                else:  # error if return type not there or doesn't match
+                    self.raise_error(f"'{active.__class__.__name__}' has an invalid return type: '{active.return_type}'")
+
+                del self.cmds[self.priority - 1]  # remove command from active
+                self.priority -= 1  # drop priority
+            # print("repeat")
+        self.resolve = True
+
+        if self.current == len(self.stream):  # stops on last command
+            self.halt = True
+
+        # print(self.current, self.var, self.holder)
+        # print(self.current, self.priority, active.__class__.__name__, active.args)
+        # print(self.priority)
+
+
+    def loop(self):
+        while not self.halt:
+            self.step()
+
+    def get_type(self, packet):  # evaluates whether a packet is a command or value
+        if type(packet) == int:  # all integers are values
+            return "val"
+        else:
+            if packet in self.list_cmd:  # strings in command list are commands
+                return "cmd"
+            else:  # if they aren't commands they are string values
+                return "val"
+
+    def get_cmd(self, packet):
+        if packet == "set":
+            return commands.Set
+        elif packet == "get":
+            return commands.Get
+        elif packet == "print":
+            return commands.Print
+        elif packet == "goto":
+            return commands.Goto
+        elif packet == "add":
+            return commands.Add
+        else:
+            self.raise_error(f"{packet} is an invalid command")
+
+    def raise_error(self, error):
+        print("Error: " + error)
+        self.halt = True
+
+
+interpreter = Interpreter("commands.txt")
+# interpreter.load(stream, var)
+
+interpreter.loop()
