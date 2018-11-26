86a04e2b57c31281f9cdf7c6dfdb2568ccb6fada
Aeolus1364 <thetesseractsshadow@gmail.com>
2018-11-26 20:07:01 +0000

    Added last method which allows code to easily execute on command completion

--- commands.py
+++ commands.py
@@ -1,24 +1,33 @@
 class Command:
     def __init__(self, interpreter, max_args):
-        self.num_args = 0  # number of arguments it takes
-        self.max_args = max_args
-        if max_args == 0:
+        self.num_args = 0  # number of arguments currently stored
+        self.max_args = max_args  # maximum arguments (if applicable)
+
+        if max_args == 0:  # determine if command accept args
             self.accept_args = False
         else:
             self.accept_args = True
+
         self.args = []  # list of actual arguments of a function
         self.flag = False  # flag turned on every time an arg is added
         self.completed = False  # command removed from stack if completed
 
-        self.inter = interpreter
+        self.inter = interpreter  # pointer to interpreter object stored in commands
 
     def argument(self, arg):
         if self.accept_args:
-            self.args.append(arg)
+            self.args.append(arg)  # add arg
             self.num_args += 1
-            self.flag = True
+            self.flag = True  # flags an arg has been added
         else:
             print(f"Error: {self.__class__.__name__} command does not accept arguments")
+
+    def last(self):  # handles the last action a command takes
+        if self.num_args == self.max_args:  # if all arguments filled
+            self.completed = True
+            return True
+        else:
+            return False
 
 
 class Set(Command):  # sets variable value in var stream
@@ -26,9 +35,8 @@ class Set(Command):  # sets variable value in var stream
         super().__init__(inter, 2)
 
     def execute(self):
-        if self.num_args == self.max_args:
+        if self.last():
             self.inter.var[self.args[0]] = self.args[1]
-            self.completed = True
 
 
 class Get(Command):  # gets variable value from var stream
@@ -36,20 +44,16 @@ class Get(Command):  # gets variable value from var stream
         super().__init__(inter, 1)
 
     def execute(self):
-        if self.num_args == self.max_args:
-            self.completed = True
+        if self.last():
             return self.inter.var[self.args[0]]  # returns arg value from var dict
 
 
 class Print(Command):
     def __init__(self, inter):
         super().__init__(inter, 1)
-        # arg 0: prints value
 
     def execute(self):
-        if self.max_args == self.num_args:
-            self.completed = True
-
+        if self.last():
             print(self.args[0])
 #
 #
@@ -78,23 +82,23 @@ class Print(Command):
 #             for c in range(inter.current, len(inter.stream)):
 #                 if c == "end"
 #
-                
+
 class End(Command):
     def __init__(self):
         super().__init__(0)
-        
+
     def evaluate(self):
-        return 1            
-          
-        
+        return 1
+
+
 class Dummy(Command):
     def __init__(self):
         super().__init__(0)
-        
+
     def evaluate(self):
         pass
-    
-    
+
+
 class Example(Command):  # example command
     def __init__(self):
         super().__init__(0)  # number of args passed in
